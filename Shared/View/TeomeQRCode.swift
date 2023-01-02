10f21e890f9b8165397a035c9ad0e719787c1909
Hadrien Barbat <hadrien@fedutia.fr>
2023-01-03 00:13:29 +0100

    various changes

--- Shared/View/TeomeQRCode.swift
+++ Shared/View/TeomeQRCode.swift
@@ -3,7 +3,7 @@ import QRCode
 
 struct TeomeQRCode: View {
     
-    // https://fedutia.fr/teome/new/13861322
+    // https://teome.fedutia.fr/new/13861322
     func createQRCode() -> Image? {
         let qrCode = QRCode(string: "WIFI:S:Teome-13861322;P:la grenouille ne pleure pas;T:WPA;;",
             size: CGSize(width: 250, height: 250)
